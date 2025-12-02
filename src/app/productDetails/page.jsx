6f7001e319fe401b78f8b4beb4a455578a6667e5
nipen026 <khantnipen@gmail.com>
2025-12-02 18:22:52 +0530

    added auth functionality

--- src/app/productDetails/page.jsx
+++ src/app/productDetails/page.jsx
@@ -1,11 +1,11 @@
-"use client";
+"use client"
 
 import { useState } from "react";
 
-import BestsellerSection from "../Components/BestsellerSection";
-import CartSidebar from "../Components/CartSidebar";
-import DownloadAppSection from "../Components/DownloadAppSection";
-import ProductDetailInfo from "../Components/ProductDetailInfo";
+import BestsellerSection from "../../Components/BestsellerSection";
+import CartSidebar from "../../Components/CartSidebar";
+import DownloadAppSection from "../../Components/DownloadAppSection";
+import ProductDetailInfo from "../../Components/ProductDetailInfo";
 
 export default function ProductDetails() {
     const [openCart, setOpenCart] = useState(false);
